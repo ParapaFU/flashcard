19d4246aadf83ed05212fbb2100d3a869237814d
Parapa <parapasndy@gmail.com>
2026-05-15 13:07:08 +0700

    menambahkan js dan mengubah sedikit file

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+// DOM
+// add card btn
+// const addCardBtn = document.getElementbyId('add-card-btn');
+const cardContainer = document.querySelector(".list-items");
+
+// Data
+let totalCard = cardContainer.children.length;
+console.log(totalCard);
+// Function
+
+// Event
+// Program
