f4ab149ab6e4f6f538fc0bc63e4021f8a00f2cb3
Parapa <parapasndy@gmail.com>
2026-05-16 16:06:46 +0700

    update

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,19 @@
 // add card btn
 // const addCardBtn = document.getElementbyId('add-card-btn');
 const cardContainer = document.querySelector(".list-items");
+let card = document.querySelector(".item")
 
 // Data
-let totalCard = cardContainer.children.length;
-console.log(totalCard);
+
+
 // Function
+function checkItems(){
+  let totalCard = cardContainer.children.length;
+  return totalCard;
+}
+
+console.log(checkItems());
+console.log(card.offsetWidth)
 
 // Event
 // Program
