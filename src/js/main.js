65dcb1452dca6d58c5945888f9d9f4097ca7a8fa
Parapa <parapasndy@gmail.com>
2026-05-17 18:13:56 +0700

    menambahkan beberapa fitur

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,173 @@
-// DOM
-// add card btn
-// const addCardBtn = document.getElementbyId('add-card-btn');
-const cardContainer = document.querySelector(".list-items");
-let card = document.querySelector(".item")
+// ===== DOM =====
+// btn tambah card
+const addCardBtn = document.getElementById("add-card-btn");
 
-// Data
+// form new card
+const formCardContainer = document.getElementById("form-card-container");
 
+// form card
+const formCard = document.getElementById("form-card");
 
-// Function
-function checkItems(){
-  let totalCard = cardContainer.children.length;
-  return totalCard;
+// semua input | form card
+const inputForms = document.querySelectorAll(".input-text");
+
+// btn create card | form card
+const createBtn = document.getElementById("create-btn");
+
+// btn cancel | form new card
+const cancelBtn = document.getElementById("cancel-btn");
+
+// container data
+const dataContainer = document.getElementById("container-data");
+
+// item | data
+const items = dataContainer.querySelectorAll(".item");
+
+// text data
+const textData = dataContainer.querySelector(".text");
+
+// ===== FUNCTION =====
+// check form
+function checkForm() {
+  const allInput = [...inputForms].every((input) => input.value.trim() !== "");
+
+  createBtn.disabled = !allInput;
 }
 
-console.log(checkItems());
-console.log(card.offsetWidth)
+// menampilkan data
+function tampilkanData() {
+  const data = JSON.parse(localStorage.getItem("itemsData")) || [];
 
-// Event
-// Program
+  dataContainer.innerHTML = "";
+
+  data.forEach((item) => {
+    dataContainer.innerHTML += `
+    <div class="item">
+          <div class="text ${item.flipped ? "active" : ""}">
+            <p class="text-front">${item.front}</p>
+            <p class="text-behind">${item.behind}</p>
+          </div>
+          <button class="delete">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.5em"
+              height="1.5em"
+              viewBox="0 0 24 24"
+            >
+              <path d="M0 0h24v24H0z" fill="none" />
+              <g
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+              >
+                <path
+                  stroke-dasharray="24"
+                  d="M12 20h5c0.5 0 1 -0.5 1 -1v-14M12 20h-5c-0.5 0 -1 -0.5 -1 -1v-14"
+                >
+                  <animate
+                    fill="freeze"
+                    attributeName="stroke-dashoffset"
+                    dur="0.5s"
+                    values="24;0"
+                  />
+                </path>
+                <path stroke-dasharray="18" stroke-dashoffset="18" d="M4 5h16">
+                  <animate
+                    fill="freeze"
+                    attributeName="stroke-dashoffset"
+                    begin="0.5s"
+                    dur="0.3s"
+                    to="0"
+                  />
+                </path>
+                <path
+                  stroke-dasharray="10"
+                  stroke-dashoffset="10"
+                  d="M10 4h4M10 9v7M14 9v7"
+                >
+                  <animate
+                    fill="freeze"
+                    attributeName="stroke-dashoffset"
+                    begin="0.8s"
+                    dur="0.2s"
+                    to="0"
+                  />
+                </path>
+              </g>
+            </svg>
+          </button>
+        </div>`;
+  });
+}
+
+// ===== EVENT =====
+// menampilkan form
+addCardBtn.addEventListener("click", () => {
+  formCardContainer.style.display = "flex";
+});
+
+// menghilangkan form
+cancelBtn.addEventListener("click", () => {
+  formCardContainer.style.display = "none";
+});
+
+// pengecekan input terisi semua
+formCard.addEventListener("input", checkForm);
+
+// mengirim data
+formCardContainer.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const frontText = document.getElementById("input-text-front").value;
+  const behindText = document.getElementById("input-text-behind").value;
+
+  const dataLama = JSON.parse(localStorage.getItem("itemsData")) || [];
+
+  dataLama.push({
+    front: frontText,
+    behind: behindText,
+    flipped: false,
+  });
+
+  localStorage.setItem("itemsData", JSON.stringify(dataLama));
+
+  formCard.reset();
+  tampilkanData();
+});
+
+// form event
+dataContainer.addEventListener("click", (e) => {
+  const deleteBtn = e.target.closest(".delete");
+  const card = e.target.closest(".item");
+
+  if (deleteBtn) {
+    const item = deleteBtn.closest(".item");
+    const allItem = [...dataContainer.querySelectorAll(".item")];
+    const index = allItem.indexOf(item);
+
+    const data = JSON.parse(localStorage.getItem("itemsData")) || [];
+    data.splice(index, 1);
+
+    localStorage.setItem("itemsData", JSON.stringify(data));
+    tampilkanData();
+  }
+
+  if (card && !deleteBtn) {
+    const text = card.querySelector(".text");
+    text.classList.toggle("active");
+
+    const allItem = [...dataContainer.querySelectorAll(".item")];
+    const index = allItem.indexOf(card);
+
+    const data = JSON.parse(localStorage.getItem("itemsData")) || [];
+    data[index].flipped = text.classList.contains("active");
+
+    localStorage.setItem("itemsData", JSON.stringify(data));
+  }
+});
+
+// ===== PROGRAM =====
+checkForm();
+tampilkanData();
